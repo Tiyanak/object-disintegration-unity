c9d96c0eb9f8fbf8d1ce36e7bb7430d764d43000
ifarszky <ifarszky@croz.net>
2018-01-15 09:52:09 +0100

    initial commit

---
+++ Assets/scripts/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject destroyedVersion;
+
+	void OnCollisionEnter(Collision collision) {
+
+		if (collision.collider.tag == "Player") {
+			Instantiate(destroyedVersion, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+
+	}
+
+}
